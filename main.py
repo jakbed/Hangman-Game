f54870548704d10b6ac6c670b295f56d0c0ced9e
Jakub Bednarczyk <122677822+sloneczny40@users.noreply.github.com>
2024-01-06 20:31:58 +0100

    ending the game

---
+++ main.py
@@ -0,0 +1,81 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+chosen_word = list(random.choice(word_list))
+lives = 6
+display = ["_" for _ in chosen_word]
+
+while "_" in display and lives > 0:
+    guess = input("Give me a letter: ").lower()
+    if guess in chosen_word:
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guess:
+                display[i] = guess
+    else:
+        lives -= 1
+        print(stages[lives+1])
+    
+    print(display)
+a
+if not "_" in display:
+    print("You win!")
+else:
+    print(stages[0])
+    print("You lose, try again.")
